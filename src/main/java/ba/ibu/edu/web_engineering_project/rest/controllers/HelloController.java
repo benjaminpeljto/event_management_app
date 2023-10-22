ab8985dafaf23f8cd36fd0366c0e2aae9554ae6a
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-10-22 17:57:10 +0200

    added UserController and routes which communicate with the user service and do basic crud operations for users collection

--- src/main/java/ba/ibu/edu/web_engineering_project/rest/controllers/HelloController.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/rest/controllers/HelloController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController()
-@RequestMapping("/api/test")
+@RestController
+@RequestMapping("api/test")
 public class HelloController {
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value="name", defaultValue="World") String name){
