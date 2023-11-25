f1707b0654cd548c5be2683f44b140012a5bdd29
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-11-25 17:34:36 +0100

    made configuration for second email service - Gmail SMTP

---
+++ src/main/java/ba/ibu/edu/web_engineering_project/rest/configuration/GmailSMTPConfiguration.java
@@ -0,0 +1,17 @@
+package ba.ibu.edu.web_engineering_project.rest.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GmailSMTPConfiguration {
+
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
+    @Bean
+    public String fromEmailGmail(){ return this.fromEmail; }
+
+
+}
