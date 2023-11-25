e6dfa2d4f3fda4207bdbe23c2ef01d1a3264f35a
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-11-25 17:48:28 +0100

    validate Token method moved from UserService to AuthService

--- src/main/java/ba/ibu/edu/web_engineering_project/core/service/UserService.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/service/UserService.java
@@ -1,22 +1,30 @@
 package ba.ibu.edu.web_engineering_project.core.service;
 
+import ba.ibu.edu.web_engineering_project.api.impl.mailsender.MailgunSender;
 import ba.ibu.edu.web_engineering_project.core.exceptions.repository.ResourceNotFoundException;
 import ba.ibu.edu.web_engineering_project.core.model.User;
 import ba.ibu.edu.web_engineering_project.core.repository.UserRepository;
 import ba.ibu.edu.web_engineering_project.rest.dto.UserDTO;
 import ba.ibu.edu.web_engineering_project.rest.dto.UserRequestDTO;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 import static java.util.stream.Collectors.toList;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-    public UserService(UserRepository userRepository) {
+    private final MailgunSender mailgunSender;
+
+    public UserService(UserRepository userRepository, MailgunSender mailgunSender) {
         this.userRepository = userRepository;
+        this.mailgunSender = mailgunSender;
     }
 
 
@@ -38,8 +46,11 @@ public class UserService {
     }
 
     public UserDTO addUser(UserRequestDTO payload){
-        User user = userRepository.save(payload.toEntity());
-        return new UserDTO(user);
+        User user = payload.toEntity();
+        user.setVerificationToken(String.valueOf(UUID.randomUUID()));
+        user.setActive(true);
+        User savedUser = userRepository.save(user);
+        return new UserDTO(savedUser);
     }
 
     public UserDTO updateUser(String id, UserRequestDTO payload){
@@ -65,4 +76,30 @@ public class UserService {
                 .orElse(null);
     }
 
+    public String validateToken(String token){
+        Optional<User> userOptional = userRepository.findByVerificationToken(token);
+
+        if(userOptional.isPresent()){
+            userOptional.get().setActive(true);
+            userRepository.save(userOptional.get());
+            return "Account successfully activated.";
+        }
+        else{
+            return "Account with provided token does not exist.";
+        }
+
+    }
+
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) {
+                return userRepository.findFirstByUsername(username)
+                        .orElseGet(() -> {
+                            return userRepository.findFirstByEmail(username)
+                                    .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+                        });
+            }
+        };
+    };
 }
