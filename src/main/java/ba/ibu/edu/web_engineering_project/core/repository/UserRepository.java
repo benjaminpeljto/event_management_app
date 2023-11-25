2fcbe4f6704de92af215a33998922c5eadba1dcb
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-11-25 17:29:59 +0100

    made a couple of custom queries needed for fetching users in different ways for authentication

--- src/main/java/ba/ibu/edu/web_engineering_project/core/repository/UserRepository.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/repository/UserRepository.java
@@ -2,10 +2,16 @@ package ba.ibu.edu.web_engineering_project.core.repository;
 
 import ba.ibu.edu.web_engineering_project.core.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User,String> {
 
     Optional<User> findFirstByEmailLike(String emailPattern);
+    Optional<User> findByVerificationToken(String verificationToken);
+    Optional<User> findByEmail(String email);
+    Optional<User> findFirstByEmail(String email);
+    Optional<User> findFirstByUsername(String username);
+    boolean existsByEmail(String email);
 }
