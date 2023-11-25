0d0c066557eb0e85811c9e61d5f10b48368ff1e5
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-11-25 19:46:36 +0100

    Added @Repository annotations which I forgot earlier

--- src/main/java/ba/ibu/edu/web_engineering_project/core/repository/TicketRepository.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/repository/TicketRepository.java
@@ -2,6 +2,8 @@ package ba.ibu.edu.web_engineering_project.core.repository;
 
 import ba.ibu.edu.web_engineering_project.core.model.Ticket;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TicketRepository extends MongoRepository<Ticket, String> {
 }
