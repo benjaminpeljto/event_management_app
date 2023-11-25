fa3f8baf177028fa33448d80583c1f910d0199c3
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-11-25 19:46:36 +0100

    Added @Repository annotations which I forgot earlier

--- src/main/java/ba/ibu/edu/web_engineering_project/core/repository/UserRepository.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/repository/UserRepository.java
@@ -2,15 +2,15 @@ package ba.ibu.edu.web_engineering_project.core.repository;
 
 import ba.ibu.edu.web_engineering_project.core.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends MongoRepository<User,String> {
 
     Optional<User> findFirstByEmailLike(String emailPattern);
     Optional<User> findByVerificationToken(String verificationToken);
-    Optional<User> findByEmail(String email);
     Optional<User> findFirstByEmail(String email);
     Optional<User> findFirstByUsername(String username);
     boolean existsByEmail(String email);
