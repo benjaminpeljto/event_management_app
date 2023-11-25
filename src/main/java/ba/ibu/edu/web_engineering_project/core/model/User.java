c93fb19bfd1e7c112830481ee4ef2fbced8b059e
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-11-25 17:44:49 +0100

    User model implements UserDetails interface for enabling spring security account authentication

--- src/main/java/ba/ibu/edu/web_engineering_project/core/model/User.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/model/User.java
@@ -3,11 +3,16 @@ package ba.ibu.edu.web_engineering_project.core.model;
 import ba.ibu.edu.web_engineering_project.core.model.enums.UserType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Document
-public class User {
+public class User implements UserDetails {
     @Id
     private String id;
     private String username;
@@ -17,6 +22,9 @@ public class User {
     private String email;
     private String password;
     private UserType userType;
+    private String verificationToken;
+    private Date TokenValidUntil;
+    private boolean active;
     private Date creationDate;
 
 
@@ -84,11 +92,63 @@ public class User {
         this.userType = userType;
     }
 
+    public String getVerificationToken() {
+        return verificationToken;
+    }
+
+    public void setVerificationToken(String verificationToken) {
+        this.verificationToken = verificationToken;
+    }
+
+    public Date getTokenValidUntil() {
+        return TokenValidUntil;
+    }
+
+    public void setTokenValidUntil(Date tokenValidUntil) {
+        TokenValidUntil = tokenValidUntil;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
     public Date getCreationDate() {
         return creationDate;
     }
 
     public void setCreationDate(Date creationDate) {
         this.creationDate = creationDate;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() { return this.active; }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(userType.name()));
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + this.lastName;
     }
 }
