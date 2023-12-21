15cd8f22e3723c2ef2bc9342d32726c4fc99f84b
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-12-21 23:17:19 +0100

    made new TicketEvent class which is embedded in the updated Ticket class and holds the quick and valuable  event information about the id of the event, name and location,

---
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/model/embedded/TicketEvent.java
@@ -0,0 +1,31 @@
+package ba.ibu.edu.web_engineering_project.core.model.embedded;
+
+public class TicketEvent {
+    private String id;
+    private String title;
+    private String location;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
