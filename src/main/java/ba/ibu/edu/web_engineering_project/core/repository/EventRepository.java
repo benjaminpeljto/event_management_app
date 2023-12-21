0b5666cccdd2c32b279a51c8117cf309930e744e
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-12-21 22:49:00 +0100

    For the correct working of the new routes for events I made custom method in the EventRepository interface for retrieving events from the DB based on their status

--- src/main/java/ba/ibu/edu/web_engineering_project/core/repository/EventRepository.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/core/repository/EventRepository.java
@@ -1,10 +1,14 @@
 package ba.ibu.edu.web_engineering_project.core.repository;
 
 import ba.ibu.edu.web_engineering_project.core.model.Event;
+import ba.ibu.edu.web_engineering_project.core.model.enums.EventStatus;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EventRepository extends MongoRepository<Event, String> {
+    List<Event> findByEventStatus(EventStatus eventStatus);
 
 }
