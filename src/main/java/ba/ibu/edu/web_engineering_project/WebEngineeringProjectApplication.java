b462c1ab8f87a17d376861b489e2bf6627d78c3e
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-12-23 22:00:08 +0100

    enabled asynchronous calls in the main application class

--- src/main/java/ba/ibu/edu/web_engineering_project/WebEngineeringProjectApplication.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/WebEngineeringProjectApplication.java
@@ -2,8 +2,10 @@ package ba.ibu.edu.web_engineering_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class WebEngineeringProjectApplication {
 
     public static void main(String[] args) {
