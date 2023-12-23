dc753cb375969f75d5a5f8b5dff815ac574ed172
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-12-23 21:58:18 +0100

    added another Bean function in LambdaAWSConfiguration class responsible for fetching the aws lambda link from application.yml file

--- src/main/java/ba/ibu/edu/web_engineering_project/rest/configuration/LambdaAWSConfiguration.java
+++ src/main/java/ba/ibu/edu/web_engineering_project/rest/configuration/LambdaAWSConfiguration.java
@@ -5,13 +5,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class EmailConfirmationLambdaConfiguration {
+public class LambdaAWSConfiguration {
 
     @Value("${aws.lambda.email-confirmation-lambda}")
     private String aws_email_confirmation_lambda;
+
+    @Value("${aws.lambda.generate-send-tickets-lambda}")
+    private String aws_generate_send_tickets_lambda;
 
     @Bean
     public String getAwsEmailConfirmationLambda() {
         return this.aws_email_confirmation_lambda;
     }
+
+    @Bean
+    public String getAwsGenerateSendTicketsLambda() { return this.aws_generate_send_tickets_lambda; }
 }
