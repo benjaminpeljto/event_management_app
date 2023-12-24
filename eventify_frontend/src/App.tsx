0f109e12d7bb6d60c94d2f285ed62a860489aa04
benjamin.peljto <benjaminpeljto00@gmail.com>
2023-12-24 23:24:13 +0100

    made an EventCard.tsx Component for listing each Event on the main page

--- eventify_frontend/src/App.tsx
+++ eventify_frontend/src/App.tsx
@@ -1,14 +1,25 @@
-import { useState } from 'react'
-import './App.css'
+import EventCard from './components/EventCard'
+import { EventForList } from './utils/types';
+
+const event: EventForList = {
+  id: "33333",
+  name: "Billiard event",
+  description: "Experience exellence in Sarajevo Billiard days",
+  location: "Zetra, Sarajevo",
+  EventCategory: "Sports",
+  occurrence: "22.12.2023",
+  totalAvailableSeats: 300
+};
 
 function App() {
   /*const [count, setCount] = useState(0)*/
 
   return (
     <>
-      
+      <EventCard event ={ event }/>
     </>
   )
 }
 
 export default App
+
