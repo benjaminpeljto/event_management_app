a2ab30354ebe9ccb858c2726a4de1edb587c4a3d
benjamin.peljto <benjaminpeljto00@gmail.com>
2024-02-13 09:12:36 +0100

    deleting front

--- eventify_frontend/src/components/EventControl/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as EventControl } from "./EventControl";
--- eventify_frontend/src/services/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EventService } from "./events.ts"
-export { default as OrderService } from "./orders.ts"
-export { default as TicketService } from "./tickets.ts"
